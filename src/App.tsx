73ec215e05acefb22af9a01d917eb7ef13753d3e
ADITYA-SM-01 <smadityaofficial@gmail.com>
2025-08-27 21:07:07 +0530

    after removing unused statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
